c43edbb008a24e1c08e83f521ba229a7748c89ed
Andrew <63522731+aloutfi@users.noreply.github.com>
2022-04-14 12:41:59 -0500

    fix: fucking CORS policy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.call_google import call_google
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="GetLatLong API", redoc_url="/", version="1.0.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/get")
